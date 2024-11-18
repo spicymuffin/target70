78677898b4a58609658b531bb677960760587eab
luigi <luigicussigh59@gmail.com>
2024-11-18 09:56:25 +0000

    asdasda

---
+++ solutions/phase3_src.d
@@ -0,0 +1,11 @@
+
+binary.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 08 01 64 55 	mov    $0x55640108,%rdi
+   7:	48 c7 04 24 97 19 40 	movq   $0x401997,(%rsp)
+   e:	00 
+   f:	c3                   	retq   
